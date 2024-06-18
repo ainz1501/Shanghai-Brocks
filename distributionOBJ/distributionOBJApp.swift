17ea29e79b8ac5357e90b96a0518934a40bb1c2d
ainz1501 <callofcthulhu2010@gmail.com>
2024-06-18 14:17:41 +0900

    Initial Commit

---
+++ distributionOBJ/distributionOBJApp.swift
@@ -0,0 +1,17 @@
+//
+//  distributionOBJApp.swift
+//  distributionOBJ
+//
+//  Created by 徳田泰地 on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct distributionOBJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
