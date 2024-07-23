e9ee810e1384d17ccb2f452068cc2963ecac6a67
ainz1501 <callofcthulhu2010@gmail.com>
2024-07-23 12:45:33 +0900

    add thumb finger

--- distributionOBJ/distributionOBJController.swift
+++ distributionOBJ/distributionOBJController.swift
@@ -21,6 +21,7 @@ class distributionOBJController: UIViewController, ARSessionDelegate {
     var viewWidth:Int = 0
     var viewHeight:Int = 0
     var box:ModelEntity!
+    var brocks:[ModelEntity?] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,25 +48,35 @@ class distributionOBJController: UIViewController, ARSessionDelegate {
         plane.generateCollisionShapes(recursive: false)
         plane.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .default, mode: .static)
         
-        box = ModelEntity(mesh: .generateBox(size: 0.05), materials: [SimpleMaterial(color: .white, isMetallic: true)])
-        box.generateCollisionShapes(recursive: false)
-        box.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .default, mode: .dynamic)
-        box.position = [0,0.025,0]
-        anchor.addChild(box)
-        arView.scene.addAnchor(anchor)
+//        box = ModelEntity(mesh: .generateBox(size: 0.05), materials: [SimpleMaterial(color: .white, isMetallic: true)])
+//        box.generateCollisionShapes(recursive: false)
+//        box.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .default, mode: .dynamic)
+//        box.position = [0,0.025,0]
+//        anchor.addChild(box)
+//        arView.scene.addAnchor(anchor)
+        brocks.append(makeBrock(arView: arView, anchor: anchor))
     }
     
     var recentIndexFingerPoint:CGPoint = .zero
+    var recentThumbFingerPoint:CGPoint = .zero
     
-    func handDetectionCompletionHandler(request: VNRequest?, error: Error?) { //どの座標を手の入力とするかを決める、多分
+    func handDetectionCompletionHandler(request: VNRequest?, error: Error?) { // インタラクション部分
         guard let observation = request?.results?.first as? VNHumanHandPoseObservation else { return }
         guard let indexFingerTip = try? observation.recognizedPoints(.all)[.indexTip],
               indexFingerTip.confidence > 0.3 else {return}
+        guard let thumbFingerTip = try? observation.recognizedPoints(.all)[.thumbTip],
+              thumbFingerTip.confidence > 0.3 else {return}
         let normalizedIndexPoint = VNImagePointForNormalizedPoint(CGPoint(x: indexFingerTip.location.y, y: indexFingerTip.location.x), viewWidth,  viewHeight)
-        if let entity = arView.entity(at: normalizedIndexPoint) as? ModelEntity, entity == box {
-            entity.addForce([0,40,0], relativeTo: nil)
-        } //ここがModelになる？
+        let normalizedThumbPoint = VNImagePointForNormalizedPoint(CGPoint(x: thumbFingerTip.location.y, y: thumbFingerTip.location.x), viewWidth,  viewHeight)
+        // 人差し指と親指の座標と重なっているオブジェクトを抽出し、それらが同じオブジェクトであるなら該当オブジェクトを代入
+        if let entity = arView.entity(at: normalizedIndexPoint) as? ModelEntity,   // 人差し指
+            let entity2 = arView.entity(at: normalizedThumbPoint) as? ModelEntity, //　親指
+            entity == brocks[0] && entity2 == brocks[0] {
+            HandtrackingHandler(entity: entity)
+        }
+        // 指の位置を更新
         recentIndexFingerPoint = normalizedIndexPoint
+        recentThumbFingerPoint = normalizedThumbPoint
     }
     
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
@@ -80,4 +91,27 @@ class distributionOBJController: UIViewController, ARSessionDelegate {
             }
         }
     }
+}
+
+// オブジェクトに触れた後のハンドラー
+func HandtrackingHandler (entity: ModelEntity) {
+    entity.addForce([0,40,0], relativeTo: nil)
+}
+
+// オブジェクト情報の設定
+func makeBrock (arView:ARView, anchor:AnchorEntity) -> ModelEntity?{
+    var brock:ModelEntity!
+    // オブジェクトの形（メッシュ）と見た目（マテリアル）
+    brock = ModelEntity(mesh: .generateBox(size: 0.05), materials: [SimpleMaterial(color: .white, isMetallic: true)])
+    // オブジェクトの衝突を検出する
+    brock.generateCollisionShapes(recursive: false)
+    // オブジェクトに物理演算を適用？
+    brock.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .default, mode: .dynamic)
+    // 座標を設定
+    brock.position = [0,0.025,0]
+    // オブジェクトをアンカーと接続
+    anchor.addChild(brock)
+    // アンカーをARViewツリーと接続
+    arView.scene.addAnchor(anchor)
+    return brock
 }
